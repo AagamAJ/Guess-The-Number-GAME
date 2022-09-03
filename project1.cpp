ec17ba9b034f6932552211ff7ff8345f31fb87da
AagamAJ <112746979+AagamAJ@users.noreply.github.com>
2022-09-03 12:07:04 +0530

    Create project1.cpp

---
+++ project1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+
+using namespace std;
+int main()
+{
+int num,guess,count=1;
+srand(time(0));
+num=rand()%100 + 1;                                  //Generates random number between 1 and 100
+// cout<<"The Number is: "<<num<<endl;
+
+cout<<"Guess the number between 1 to 100 \n";
+
+do
+{
+    cin>>guess;
+    if(guess>num)
+    {
+        cout<<"Lower Number please! \n";
+    }
+    else if(guess<num)
+    {
+        cout<<"Higher Number please! \n";
+    }
+    else
+    {
+        cout<<"You guessed it in "<<count<<" attempts \n";
+    }
+    count++;
+} while (guess!=num);
+    return 0;
+}
